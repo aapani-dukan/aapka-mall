1878eec946d36c612ee8ea9e672b858283e551bf
aapani-dukan <shivraj8404@gmail.com>
2025-06-01 16:48:17 +0530

    Update tooltip.tsx

--- client/src/components/ui/tooltip.tsx
+++ client/src/components/ui/tooltip.tsx
@@ -25,5 +25,5 @@ const TooltipContent = React.forwardRef<
 ))
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
+//export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
 
