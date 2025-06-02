27ec0ca6a3fd0455b01129867c427cde9feecb55
aapani-dukan <shivraj8404@gmail.com>
2025-06-03 01:33:32 +0530

    Update build-server.ts

--- build-server.ts
+++ build-server.ts
@@ -1,10 +1,1 @@
-import { build } from "esbuild";
-
-build({
-  entryPoints: ["server/index.ts"],
-  bundle: true,
-  platform: "node",
-  format: "esm",
-  outdir: "dist",
-  packages: "external",
-}).catch(() => process.exit(1));
+import "./server/index";
