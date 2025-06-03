3f2cda7b23535dda27a59ea349ff5bb9a2a0cc1d
aapani-dukan <shivraj8404@gmail.com>
2025-06-03 11:24:23 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -38,7 +38,9 @@ app.use((req, res, next) => {
 
 (async () => {
   const server = await registerRoutes(app);
-
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).send("✅ Shopnish server is live!");
+});
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
