b9892ce47a0fbdc289911de1956e75d847bc430f
aapani-dukan <shivraj8404@gmail.com>
2025-06-03 20:12:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// ES module-compatible way to get __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   root: path.resolve(__dirname, "client"),
